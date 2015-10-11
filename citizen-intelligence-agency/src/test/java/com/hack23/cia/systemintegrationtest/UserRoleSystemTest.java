929891aa9291a6640c6016c80ff4aa8b9d51e09a
pether.sorling@gmail.com <pether.sorling@gmail.com>
2015-10-11 21:55:14 +0200

    disable chrome test

--- citizen-intelligence-agency/src/test/java/com/hack23/cia/systemintegrationtest/UserRoleSystemTest.java
+++ citizen-intelligence-agency/src/test/java/com/hack23/cia/systemintegrationtest/UserRoleSystemTest.java
@@ -88,7 +88,7 @@ public class UserRoleSystemTest extends AbstractSystemTest {
 	 */
 	@Parameters(name = "SiteTest{index}: browser({0})")
 	public static Collection<String[]> browsersStrings() {
-		return Arrays.asList(new String[][] { { "firefox" }, { "chrome" } });
+		return Arrays.asList(new String[][] { { "firefox" } });
 		// return Arrays.asList(new Object[][] { { "firefox" },{ "chrome" }, {
 		// "htmlunit-firefox" },{ "htmlunit-ie11" },{ "htmlunit-chrome" } });
 	}
