9a8788ce75f2a5dcd9b8d1e35eb67c6e990bf2dd
pether.sorling@gmail.com <pether.sorling@gmail.com>
2015-12-31 16:55:47 +0100

    cleanup

---
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/labelfactory/LabelFactory.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2014 James Pether Sörling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *	$Id$
+ *  $HeadURL$
+*/
+package com.hack23.cia.web.impl.ui.application.views.common.labelfactory;
+
+import com.vaadin.ui.Label;
+import com.vaadin.ui.themes.Reindeer;
+
+public final class LabelFactory {
+
+	public static Label createHeader2Label(final String content) {
+		final Label label = new Label(content);
+		label.addStyleName(Reindeer.LABEL_H2);
+		return label;
+	}
+
+}
