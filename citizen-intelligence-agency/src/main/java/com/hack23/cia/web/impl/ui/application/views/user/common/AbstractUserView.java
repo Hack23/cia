997954cca8111cbb449ae8cd5ac5211837d23594
pether.sorling@gmail.com <pether.sorling@gmail.com>
2016-04-28 22:47:08 +0200

    cleanup

--- citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/user/common/AbstractUserView.java
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/user/common/AbstractUserView.java
@@ -20,18 +20,8 @@ package com.hack23.cia.web.impl.ui.application.views.user.common;
 
 import java.util.Map;
 
-import org.dussan.vaadin.dcharts.DCharts;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.hack23.cia.web.impl.ui.application.action.PageActionEventHelper;
 import com.hack23.cia.web.impl.ui.application.views.common.AbstractView;
-import com.hack23.cia.web.impl.ui.application.views.common.chartfactory.AdminChartDataManager;
-import com.hack23.cia.web.impl.ui.application.views.common.labelfactory.LabelFactory;
-import com.hack23.cia.web.impl.ui.application.views.common.pagelinks.PageLinkFactory;
 import com.hack23.cia.web.impl.ui.application.views.common.pagemode.PageModeContentFactory;
-import com.hack23.cia.web.impl.ui.application.views.common.sizing.ContentRatio;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
 
 /**
  * The Class AbstractUserView.
@@ -39,27 +29,9 @@ import com.vaadin.ui.VerticalLayout;
 public abstract class AbstractUserView extends AbstractView {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
-
-	/** The Constant GENERAL_PAGE_MODE_PAGE_VISIT. */
-	public static final String GENERAL_PAGE_MODE_PAGE_VISIT = "General Page Mode Page Visit";
-
-	/** The Constant CURRENT_PAGE_VISIT_HISTORY. */
-	public static final String CURRENT_PAGE_VISIT_HISTORY = "Current Page Visit History";
-
-	/** The page link factory. */
-	@Autowired
-	protected transient PageLinkFactory pageLinkFactory;
-
-	/** The page action event helper. */
-	@Autowired
-	protected transient PageActionEventHelper pageActionEventHelper;
-
-	/** The admin chart data manager. */
-	@Autowired
-	private transient AdminChartDataManager adminChartDataManager;
 
 	/**
 	 * Instantiates a new abstract user view.
@@ -69,37 +41,9 @@ public abstract class AbstractUserView extends AbstractView {
 	 * @param pageName
 	 *            the page name
 	 */
-	protected AbstractUserView(final Map<String, PageModeContentFactory> pageModeContentFactoryMap, final String pageName) {
-		super(pageModeContentFactoryMap,pageName);
-	}
-
-
-
-	/**
-	 * Creates the page visit history.
-	 *
-	 * @param pageName
-	 *            the page name
-	 * @param pageId
-	 *            the page id
-	 * @param panelContent
-	 *            the panel content
-	 */
-	protected final void createPageVisitHistory(final String pageName, final String pageId, final VerticalLayout panelContent) {
-		final Label createHeader2Label = LabelFactory.createHeader2Label(CURRENT_PAGE_VISIT_HISTORY);
-		panelContent.addComponent(createHeader2Label);
-		final DCharts createApplicationActionEventPageElementDailySummaryChart = adminChartDataManager.createApplicationActionEventPageElementDailySummaryChart(pageName,pageId);
-		panelContent.addComponent(createApplicationActionEventPageElementDailySummaryChart);
-
-		final Label createHeader2Label2 = LabelFactory.createHeader2Label(GENERAL_PAGE_MODE_PAGE_VISIT);
-		panelContent.addComponent(createHeader2Label2);
-		final DCharts createApplicationActionEventPageModeDailySummaryChart = adminChartDataManager.createApplicationActionEventPageModeDailySummaryChart(pageName);
-		panelContent.addComponent(createApplicationActionEventPageModeDailySummaryChart);
-
-		panelContent.setExpandRatio(createHeader2Label,ContentRatio.SMALL);
-		panelContent.setExpandRatio(createApplicationActionEventPageElementDailySummaryChart, ContentRatio.GRID);
-		panelContent.setExpandRatio(createHeader2Label2,ContentRatio.SMALL);
-		panelContent.setExpandRatio(createApplicationActionEventPageModeDailySummaryChart, ContentRatio.GRID);
+	protected AbstractUserView(final Map<String, PageModeContentFactory> pageModeContentFactoryMap,
+			final String pageName) {
+		super(pageModeContentFactoryMap, pageName);
 	}
 
 }
