c01183baf291a8113f75ef552139b9ef56603a14
pether.sorling@gmail.com <pether.sorling@gmail.com>
2016-10-21 21:02:14 +0200

    Uses data from http://sromyndigheter.esv.se/

--- service.external.esv/src/main/java/com/hack23/cia/service/external/esv/impl/EsvApiImpl.java
+++ service.external.esv/src/main/java/com/hack23/cia/service/external/esv/impl/EsvApiImpl.java
@@ -18,11 +18,21 @@
 */
 package com.hack23.cia.service.external.esv.impl;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.hack23.cia.service.external.esv.api.EsvApi;
+import com.hack23.cia.service.external.esv.api.GovernmentBodyAnnualSummary;
 
 /**
  * The Class EsvApiImpl.
@@ -31,15 +41,56 @@ import com.hack23.cia.service.external.esv.api.EsvApi;
 public final class EsvApiImpl implements EsvApi {
 
 	/** The Constant LOGGER. */
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(EsvApiImpl.class);
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(EsvApiImpl.class);
 
 	/**
 	 * Instantiates a new val api impl.
 	 */
 	public EsvApiImpl() {
 		super();
+	}
+
+	@Override
+	public List<List<GovernmentBodyAnnualSummary>> getGovernmentBodyAnnualSummaryData() {
+
+		final List<List<GovernmentBodyAnnualSummary>> list = new ArrayList<>();
+		try {
+			final HSSFWorkbook myWorkBook = new HSSFWorkbook(
+					EsvApiImpl.class.getResourceAsStream("/Myndighetsinformation.xls"));
+
+			for (int sheetNr = 0; sheetNr < myWorkBook.getNumberOfSheets(); sheetNr++) {
+				final HSSFSheet mySheet = myWorkBook.getSheetAt(sheetNr);
+				mySheet.getSheetName();
+
+				final Iterator<Row> rowIterator = mySheet.iterator();
+
+				rowIterator.next();
+
+				while (rowIterator.hasNext()) {
+					final Row row = rowIterator.next();
+
+					final List<GovernmentBodyAnnualSummary> yearList = new ArrayList<>();
+
+					final short minColIx = row.getFirstCellNum();
+					final short maxColIx = row.getLastCellNum();
+					for (short colIx = minColIx; colIx < maxColIx; colIx++) {
+						final Cell cell = row.getCell(colIx);
+						if (cell == null) {
+							continue;
+						}
+						System.out.println(cell.toString());
+					}
+
+					list.add(yearList);
+				}
+			}
+
+			myWorkBook.close();
+		} catch (final IOException e) {
+			LOGGER.warn("Problem loading", e);
+		}
+
+		return list;
 	}
 
 }
