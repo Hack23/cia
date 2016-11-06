9915439f534a9019a64e183be293fedf75bd30d6
pether.sorling@gmail.com <pether.sorling@gmail.com>
2016-11-07 00:20:01 +0100

    government body headcount charts work.

---
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/chartfactory/api/GovernmentBodyChartDataManager.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2014 James Pether Sörling
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *	$Id$
+ *  $HeadURL$
+*/
+package com.hack23.cia.web.impl.ui.application.views.common.chartfactory.api;
+
+import com.vaadin.ui.AbstractOrderedLayout;
+
+/**
+ * The Interface GovernmentBodyChartDataManager.
+ */
+public interface GovernmentBodyChartDataManager {
+
+	/**
+	 * Creates the government body headcount chart.
+	 *
+	 * @param content
+	 *            the content
+	 * @param name
+	 *            the name
+	 */
+	void createGovernmentBodyHeadcountChart(AbstractOrderedLayout content,String name);
+	
+	/**
+	 * Creates the ministry government body headcount summary chart.
+	 *
+	 * @param content
+	 *            the content
+	 * @param name
+	 *            the name
+	 */
+	void createMinistryGovernmentBodyHeadcountSummaryChart(AbstractOrderedLayout content,String name);
+
+	/**
+	 * Creates the ministry government body headcount summary chart.
+	 *
+	 * @param content
+	 *            the content
+	 */
+	void createMinistryGovernmentBodyHeadcountSummaryChart(AbstractOrderedLayout content);
+
+}
---
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/chartfactory/impl/GovernmentBodyChartDataManagerImpl.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2014 James Pether Sörling
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *	$Id$
+ *  $HeadURL$
+*/
+package com.hack23.cia.web.impl.ui.application.views.common.chartfactory.impl;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import org.dussan.vaadin.dcharts.DCharts;
+import org.dussan.vaadin.dcharts.base.elements.XYseries;
+import org.dussan.vaadin.dcharts.data.DataSeries;
+import org.dussan.vaadin.dcharts.options.Series;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hack23.cia.service.external.esv.api.EsvApi;
+import com.hack23.cia.service.external.esv.api.GovernmentBodyAnnualSummary;
+import com.hack23.cia.web.impl.ui.application.views.common.chartfactory.api.GovernmentBodyChartDataManager;
+import com.vaadin.ui.AbstractOrderedLayout;
+
+/**
+ * The Class GovernmentBodyChartDataManagerImpl.
+ */
+@Service
+public class GovernmentBodyChartDataManagerImpl extends AbstractChartDataManagerImpl
+		implements GovernmentBodyChartDataManager {
+
+	@Autowired
+	private EsvApi esvApi;
+
+	/**
+	 * Instantiates a new government body chart data manager impl.
+	 */
+	public GovernmentBodyChartDataManagerImpl() {
+		super();
+	}
+
+	@Override
+	public void createGovernmentBodyHeadcountChart(AbstractOrderedLayout content, String name) {
+		final Map<Integer, GovernmentBodyAnnualSummary> map = esvApi.getDataPerGovernmentBody(name);
+
+		final DataSeries dataSeries = new DataSeries();
+
+		final Series series = new Series();
+
+		series.addSeries(new XYseries().setLabel(name));
+
+		dataSeries.newSeries();
+
+		for (final Entry<Integer, GovernmentBodyAnnualSummary> entry : map.entrySet()) {
+			GovernmentBodyAnnualSummary item = entry.getValue();
+			if (entry.getKey() != null) {
+				if (item != null && item.getHeadCount() > 0) {
+					dataSeries.add("01-JAN-" + item.getYear(), item.getHeadCount());
+				}
+			}
+		}
+				
+		addChart(content,name + " Annual headcount summary", new DCharts().setDataSeries(dataSeries).setOptions(ChartOptionsImpl.INSTANCE.createOptionsXYDateFloatLegendOutside(series)).show());		
+	}
+
+	@Override
+	public void createMinistryGovernmentBodyHeadcountSummaryChart(AbstractOrderedLayout content, String name) {
+		final Map<Integer, List<GovernmentBodyAnnualSummary>> map = esvApi.getDataPerMinistry(name);
+
+		final DataSeries dataSeries = new DataSeries();
+
+		final Series series = new Series();
+
+		series.addSeries(new XYseries().setLabel(name));
+
+		dataSeries.newSeries();
+
+		for (final Entry<Integer, List<GovernmentBodyAnnualSummary>> entry : map.entrySet()) {
+
+			List<GovernmentBodyAnnualSummary> item = entry.getValue();
+			Integer totalHeadcount = item.stream().collect(Collectors.summingInt(p -> p.getHeadCount()));
+			
+			if (entry.getKey() != null) {
+				if (item != null && totalHeadcount > 0) {
+					dataSeries.add("01-JAN-" + entry.getKey(), totalHeadcount);
+				}
+
+			}
+		}
+
+		addChart(content, name + " Annual headcount summary, all government bodies", new DCharts().setDataSeries(dataSeries).setOptions(ChartOptionsImpl.INSTANCE.createOptionsXYDateFloatLegendOutside(series)).show());		
+		
+	}
+
+	@Override
+	public void createMinistryGovernmentBodyHeadcountSummaryChart(AbstractOrderedLayout content) {
+		final Map<Integer, List<GovernmentBodyAnnualSummary>> map = esvApi.getData();
+
+		final DataSeries dataSeries = new DataSeries();
+
+		final Series series = new Series();
+
+		series.addSeries(new XYseries().setLabel("All Ministries"));
+
+		dataSeries.newSeries();
+
+		for (final Entry<Integer, List<GovernmentBodyAnnualSummary>> entry : map.entrySet()) {
+
+			List<GovernmentBodyAnnualSummary> item = entry.getValue();
+			Integer totalHeadcount = item.stream().collect(Collectors.summingInt(p -> p.getHeadCount()));
+			
+			if (entry.getKey() != null) {
+				if (item != null && totalHeadcount > 0) {
+					dataSeries.add("01-JAN-" + entry.getKey(), totalHeadcount);
+				}
+
+			}
+		}
+
+		addChart(content, "Annual headcount summary, all ministries", new DCharts().setDataSeries(dataSeries).setOptions(ChartOptionsImpl.INSTANCE.createOptionsXYDateFloatLegendOutside(series)).show());		
+
+	}
+	
+}
