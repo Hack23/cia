5ffa6d7e893321ea8f03cee5d26d350ec791f98c
pether.sorling@gmail.com <pether.sorling@gmail.com>
2017-01-21 11:22:10 +0100

    increase perf test requirements

--- service.impl/src/test/java/com/hack23/cia/service/impl/action/application/LogoutServiceITest.java
+++ service.impl/src/test/java/com/hack23/cia/service/impl/action/application/LogoutServiceITest.java
@@ -64,7 +64,7 @@ public final class LogoutServiceITest extends AbstractServiceFunctionalIntegrati
 	 */
 	@Test
 	@PerfTest(threads = 4, duration = 3000, warmUp = 1500)
-	@Required(max = 1200, average = 800, percentile95 = 1000, throughput = 4)
+	@Required(max = 1200, average = 950, percentile95 = 1000, throughput = 4)
 	public void serviceLogoutRequestSuccessTest() throws Exception {
 		final CreateApplicationSessionRequest createApplicationSesstion = createApplicationSesstionWithRoleAnonymous();
 
