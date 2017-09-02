9ae6baf28d96401672a931dc88091ac353ff608c
pether.sorling@gmail.com <pether.sorling@gmail.com>
2017-09-02 19:59:54 +0200

    jdk9 wip

---
+++ service.api/src/main/java/com.hack23.cia.service.api/module-info.java
@@ -0,0 +1,27 @@
+module com.hack23.cia.service.api {
+
+	exports com.hack23.cia.service.api;
+
+	requires commons.lang;
+
+	requires com.hack23.cia.service.data.api;
+
+	requires model.internal.application.user.impl;
+
+	requires model.external.val.riksdagsvalkrets.impl;
+	requires model.external.riksdagen.voteringlista.impl;
+	requires model.external.val.kommunvalkrets.impl;
+	requires model.external.worldbank.topic.impl;
+	requires model.external.worldbank.indicators.impl;
+	requires model.external.riksdagen.documentcontent.impl;
+	requires model.external.worldbank.data.impl;
+	requires model.external.riksdagen.person.impl;
+	requires model.external.riksdagen.dokumentstatus.impl;
+	requires model.external.riksdagen.dokumentlista.impl;
+	requires model.external.riksdagen.utskottsforslag.impl;
+	requires model.external.val.partier.impl;
+	requires model.external.riksdagen.personlista.impl;
+	requires model.external.val.landstingvalkrets.impl;
+	requires model.external.riksdagen.votering.impl;
+
+}
