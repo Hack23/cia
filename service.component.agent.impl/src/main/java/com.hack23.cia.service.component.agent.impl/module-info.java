f89d2872aff2d3228131a4b83643423aea5454fe
pether.sorling@gmail.com <pether.sorling@gmail.com>
2017-09-03 17:23:07 +0200

    prepare jdk9

---
+++ service.component.agent.impl/src/main/java/com.hack23.cia.service.component.agent.impl/module-info.java
@@ -0,0 +1,46 @@
+module com.hack23.cia.service.component.agent.impl {
+
+
+	exports com.hack23.cia.service.component.agent.impl.command;
+	exports com.hack23.cia.service.component.agent.impl.common;
+	exports com.hack23.cia.service.component.agent.impl.riksdagen;
+	exports com.hack23.cia.service.component.agent.impl.val;
+	exports com.hack23.cia.service.component.agent.impl.worldbank;
+
+	requires java.xml.bind;
+	requires java.xml.ws.annotation;
+	requires slf4j.api;
+	requires spring.context;
+	requires spring.beans;
+	requires spring.tx;
+	requires spring.jms;
+	requires org.apache.commons.lang3;
+
+
+	requires joda.time;
+
+	requires jms;
+
+	requires com.hack23.cia.service.external.common;
+    requires com.hack23.cia.service.data.api;
+	requires com.hack23.cia.service.component.agent.api;
+	requires com.hack23.cia.service.external.riksdagen;
+	requires com.hack23.cia.service.external.val;
+	requires com.hack23.cia.service.external.worldbank;
+
+	requires model.external.val.riksdagsvalkrets.impl;
+	requires model.external.riksdagen.voteringlista.impl;
+	requires model.external.val.kommunvalkrets.impl;
+	requires model.external.worldbank.topic.impl;
+	requires model.external.worldbank.indicators.impl;
+	requires model.external.riksdagen.documentcontent.impl;
+	requires model.external.worldbank.data.impl;
+	requires model.external.riksdagen.person.impl;
+	requires model.external.riksdagen.dokumentstatus.impl;
+	requires model.external.riksdagen.dokumentlista.impl;
+	requires model.external.riksdagen.utskottsforslag.impl;
+	requires model.external.val.partier.impl;
+	requires model.external.riksdagen.personlista.impl;
+	requires model.external.val.landstingvalkrets.impl;
+	requires model.external.riksdagen.votering.impl;
+}
--- service.component.agent.impl/src/main/java/com/hack23/cia/service/component/agent/impl/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.hack23.cia.service.component.agent.impl {
-	exports com.hack23.cia.service.component.agent.impl;
-	requires com.hack23.cia.service.component.agent.api;
-	requires com.hack23.cia.service.data.api;	
-	requires com.hack23.cia.service.external.riksdagen;
-	requires com.hack23.cia.service.external.val;
-	requires com.hack23.cia.service.external.worldbank;
-	requires com.hack23.cia.service.external.vdem;		
-}
