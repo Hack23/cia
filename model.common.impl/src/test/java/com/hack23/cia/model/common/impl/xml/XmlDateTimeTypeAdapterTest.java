5773c0486a23b5b2a1f3cadce2651476d327fa3d
pether.sorling@gmail.com <pether.sorling@gmail.com>
2017-09-04 18:12:43 +0200

    restore old test

---
+++ model.common.impl/src/test/java/com/hack23/cia/model/common/impl/xml/XmlDateTimeTypeAdapterTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2010 James Pether Sörling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *	$Id$
+ *  $HeadURL$
+*/
+package com.hack23.cia.model.common.impl.xml;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.hack23.cia.testfoundation.AbstractTest;
+
+/**
+ * The Class XmlDateTimeTypeAdapterTest.
+ */
+public final class XmlDateTimeTypeAdapterTest extends AbstractTest {
+
+
+	/**
+	 * Parses the date time test.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@Test
+	public void parseDateTimeTest() throws Exception {
+		String printTime = "2015-05-21T00:33:27.560+02:00";
+		XmlDateTimeTypeAdapter xmlDateTimeTypeAdapter = new XmlDateTimeTypeAdapter();
+		Date simpleSwedishDateForm = xmlDateTimeTypeAdapter.unmarshal(printTime);
+		assertEquals("Expect specified time",printTime,xmlDateTimeTypeAdapter.marshal(simpleSwedishDateForm));
+
+		assertNull("Expect null",xmlDateTimeTypeAdapter.marshal(null));
+		assertNull("Expect null",xmlDateTimeTypeAdapter.unmarshal(null));
+
+	}
+
+}
---
+++ model.common.impl/src/test/java/com/hack23/cia/model/common/impl/xml/XmlDateTypeAdapterTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2010 James Pether Sörling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *	$Id$
+ *  $HeadURL$
+*/
+package com.hack23.cia.model.common.impl.xml;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.hack23.cia.testfoundation.AbstractTest;
+
+/**
+ * The Class XmlDateTypeAdapterTest.
+ */
+public final class XmlDateTypeAdapterTest extends AbstractTest {
+
+	/**
+	 * Parses the date test.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@Test
+	public void parseDateTest() throws Exception {
+
+		XmlDateTypeAdapter xmlDateTypeAdapter = new XmlDateTypeAdapter();
+		Date simpleSwedishDateForm = xmlDateTypeAdapter.unmarshal("2015-05-20");
+		assertNotNull("Expect a result",simpleSwedishDateForm);
+		assertEquals("Expect specified date","2015-05-20+02:00",xmlDateTypeAdapter.marshal(simpleSwedishDateForm));
+
+		Date simpleSwedishDateTimeFormat = xmlDateTypeAdapter.unmarshal("2001-01-01 00:00:00");
+		assertNotNull("Expect a result",simpleSwedishDateTimeFormat);
+		assertEquals("Expect specified date","2001-01-01+01:00",xmlDateTypeAdapter.marshal(simpleSwedishDateTimeFormat));
+
+
+		Date validDateForm = xmlDateTypeAdapter.unmarshal("2001-01-01+01:00");
+		assertNotNull("Expect specified date",validDateForm);
+		assertEquals("2001-01-01+01:00",xmlDateTypeAdapter.marshal(validDateForm));
+
+		assertNull("Expect null",xmlDateTypeAdapter.marshal(null));
+		assertNull("Expect null",xmlDateTypeAdapter.unmarshal(null));
+
+	}
+
+
+}
