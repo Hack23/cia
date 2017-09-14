bcfe050c0c8629ed36d46160df63ae9bb61ccc9a
pether.sorling@gmail.com <pether.sorling@gmail.com>
2017-09-14 20:16:26 +0200

    cleanup

--- service.impl/src/main/java/com/hack23/cia/service/impl/task/AbstractJob.java
+++ service.impl/src/main/java/com/hack23/cia/service/impl/task/AbstractJob.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2010 James Pether Sörling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *	$Id$
+ *  $HeadURL$
+*/
 package com.hack23.cia.service.impl.task;
 
 import java.io.Serializable;
@@ -11,16 +29,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+/**
+ * The Class AbstractJob.
+ */
 public abstract class AbstractJob extends QuartzJobBean implements Serializable{
 
+	/** The Constant LOGGER. */
 	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractJob.class);
 
+	/** The Constant APPLICATION_CONTEXT. */
 	private static final String APPLICATION_CONTEXT = "applicationContext";
-	/**
-	 *
-	 */
+
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Gets the job context holder.
+	 *
+	 * @param jobContext
+	 *            the job context
+	 * @return the job context holder
+	 */
 	protected final JobContextHolder getJobContextHolder(final JobExecutionContext jobContext) {
 		final Scheduler scheduler = jobContext.getScheduler();
 		    JobContextHolder bean = null;
