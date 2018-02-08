bbaff83608ccd330d40a94006f5cb53d6e9ca264
pether.sorling@gmail.com <pether.sorling@gmail.com>
2018-02-08 01:10:39 +0100

    org.hibernate 5.2.12.Final -> 5.3.0.Beta2 hibernate-search 5.9.0.Final -> 5.10.0-SNAPSHOT(jpa 2.2 support)

--- service.data.api/src/main/java/com.hack23.cia.service.data.api/module-info.java
+++ service.data.api/src/main/java/com.hack23.cia.service.data.api/module-info.java
@@ -2,7 +2,7 @@ module com.hack23.cia.service.data.api {
 	exports com.hack23.cia.service.data.api;
 
 	requires java.se.ee;
-	requires hibernate.jpa;
+	requires java.persistence;
 
 	requires model.internal.application.user.impl;
 
