d87c9d40118995e24138e670a51ddec1da760525
pether.sorling@gmail.com <pether.sorling@gmail.com>
2018-02-24 00:27:04 +0100

    https://github.com/Hack23/cia/issues/8 test

--- service.impl/src/test/java/com/hack23/cia/service/impl/rules/RuleServiceITest.java
+++ service.impl/src/test/java/com/hack23/cia/service/impl/rules/RuleServiceITest.java
@@ -18,8 +18,9 @@
 */
 package com.hack23.cia.service.impl.rules;
 
-import java.util.List;
+import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
@@ -30,44 +31,61 @@ import org.kie.api.runtime.KieSession;
 import org.kie.internal.io.ResourceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.hack23.cia.model.external.riksdagen.person.impl.PersonData;
-import com.hack23.cia.service.data.api.PersonDataDAO;
+import com.hack23.cia.model.internal.application.data.party.impl.ViewRiksdagenPartySummary;
+import com.hack23.cia.model.internal.application.data.politician.impl.ViewRiksdagenPolitician;
+import com.hack23.cia.service.api.ApplicationManager;
+import com.hack23.cia.service.api.DataContainer;
 import com.hack23.cia.service.impl.AbstractServiceFunctionalIntegrationTest;
 
 public final class RuleServiceITest extends AbstractServiceFunctionalIntegrationTest {
 
-	private static final String drlFile = "rules/Politician.drl";
+	private static final String politicianDrlFile = "rules/Politician.drl";
 	
+	private static final String partyDrlFile = "rules/Party.drl";
+		
 	@Autowired
-	private PersonDataDAO PersonData;
+	private ApplicationManager applicationManager;
 	
+	@Before
+	public void authAdminUser() throws IOException {
+		setAuthenticatedAdminuser();
+	}
+
 	@Test
 	public void ruleEngineTest() throws Exception {
 		KieServices kieServices = KieServices.Factory.get();
 
 		KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-		kieFileSystem.write(ResourceFactory.newClassPathResource(drlFile));
+		kieFileSystem.write(ResourceFactory.newClassPathResource(politicianDrlFile));
+		kieFileSystem.write(ResourceFactory.newClassPathResource(partyDrlFile));
+		
 		KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
 		kieBuilder.buildAll();
 		KieModule kieModule = kieBuilder.getKieModule();
 
 		KieContainer newKieContainer = kieServices.newKieContainer(kieModule.getReleaseId());
 		
-		
-		List<com.hack23.cia.model.external.riksdagen.person.impl.PersonData> all = PersonData.getAll();
+		final DataContainer<ViewRiksdagenPolitician, String> politicianDataContainer = applicationManager.getDataContainer(ViewRiksdagenPolitician.class);
 
-		KieSession ksession = newKieContainer.newKieSession();
-		for (PersonData personData : all) {
-			
-			ksession.insert( personData );
+		final DataContainer<ViewRiksdagenPartySummary, String> partyDataContainer = applicationManager.getDataContainer(ViewRiksdagenPartySummary.class);
+
+		for (ViewRiksdagenPolitician politicianData : politicianDataContainer.getAll()) {
+			if (politicianData != null) {
+				KieSession ksession = newKieContainer.newKieSession();			
+				ksession.insert( politicianData );
+				System.out.println("Politician Rules fired:" +ksession.fireAllRules() + ":" + politicianData.getFirstName() + " " +politicianData.getLastName());
+				ksession.dispose();
+			}
 		}
-		System.out.println(ksession.fireAllRules());
-		ksession.dispose();
-		
-        
 
-
-
+		for (ViewRiksdagenPartySummary partyData : partyDataContainer.getAll()) {
+			if (partyData != null) {
+				KieSession ksession = newKieContainer.newKieSession();			
+				ksession.insert( partyData );
+				System.out.println("Pary Rules fired:" +ksession.fireAllRules() + ":" + partyData.getParty());
+				ksession.dispose();
+			}
+		}
 
 	}
 
