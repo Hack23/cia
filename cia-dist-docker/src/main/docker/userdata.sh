060a288697ad6704f8195c29b08b90a5c3ca5b97
pether.sorling@gmail.com <pether.sorling@gmail.com>
2018-05-10 14:36:39 +0200

    prepare docker demo image

--- cia-dist-docker/src/main/docker/userdata.sh
+++ cia-dist-docker/src/main/docker/userdata.sh
@@ -19,14 +19,62 @@ locale-gen en_US.UTF-8 en_GB.UTF-8 sv_SE.UTF-8
 dpkg-reconfigure --frontend=noninteractive locales 
 
 #
+#
+# 
+apt-get -y install postgresql-10 postgresql-10-pgaudit postgresql-contrib
+service postgresql stop
+
+echo "host all all ::1/128 md5" >> /etc/postgresql/10/main/pg_hba.conf
+
+openssl rand -base64 48 > passphrase.txt
+openssl genrsa -des3 -passout file:passphrase.txt -out server.pass.key 2048
+openssl rsa -passin file:passphrase.txt -in server.pass.key -out server.key
+rm server.pass.key
+openssl req -new -key server.key -out server.csr -subj "/C=UK/ST=Postgresqll/L=Docker/O=Hack23/OU=demo/CN=localhost"
+openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
+rm passphrase.txt
+rm server.csr
+
+cp server.crt /var/lib/postgresql/10/main/server.crt
+cp server.key /var/lib/postgresql/10/main/server.key
+rm server.key
+chmod 700 /var/lib/postgresql/10/main/server.*
+chown postgres:postgres /var/lib/postgresql/10/main/server.*
+
+# /etc/postgresql/10/main/postgresql.conf
+## ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+## ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'
+# max_prepared_transactions = 100 
+
+### shared_preload_libraries = 'pg_stat_statements, pgaudit, pgcrypto' 
+### pgaudit.log = ddl 
+### pg_stat_statements.track = all 
+### pg_stat_statements.max = 10000
+
+service postgresql stop
+
+su - postgres -c "psql -c 'CREATE USER eris WITH password 'discord';'"
+su - postgres -c "psql -c 'CREATE DATABASE cia_dev;'"
+su - postgres -c "psql -c 'GRANT ALL PRIVILEGES ON DATABASE cia_dev to eris;'"
+
+#
 # INSTALL ORACLE JDK
 #
-apt-get -y install openjdk-8-jdk-headless software-properties-common
-add-apt-repository ppa:webupd8team/java
+apt-get -y install software-properties-common ca-certificates-java
+add-apt-repository ppa:linuxuprising/java
 apt-get update
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt-get -y install oracle-java8-installer
-apt-get -y install oracle-java8-unlimited-jce-policy
-apt-get -y install oracle-java8-set-default
+echo oracle-java10-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get -y install oracle-java10-installer
+dpkg -i /root/cia-dist-deb.deb
+
+mkdir /opt/cia/.postgresql
+cp server.crt /opt/cia/.postgresql/root.crt
+chmod 700 /opt/cia/.postgresql/root.crt
+chown -R cia:cia /opt/cia/.postgresql/root.crt
+rm servert.crt
+
+apt-get -y autoremove
+apt-get -y autoclean
+rm /root/cia-dist-deb.deb
 
 echo completed
