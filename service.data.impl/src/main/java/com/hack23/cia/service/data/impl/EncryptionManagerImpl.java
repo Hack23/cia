5cf6827caa075400d514108b28e9572713753d6c
pether.sorling@gmail.com <pether.sorling@gmail.com>
2018-05-14 06:56:14 +0200

    fix test

--- service.data.impl/src/main/java/com/hack23/cia/service/data/impl/EncryptionManagerImpl.java
+++ service.data.impl/src/main/java/com/hack23/cia/service/data/impl/EncryptionManagerImpl.java
@@ -48,7 +48,7 @@ final class EncryptionManagerImpl implements EncryptionManager {
 	@Override
 	public void setEncryptionKey(final String key) {
 		final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-		jdbcTemplate.queryForList("select set_config('cia.encrypt.key',?, false)",new Object[] { key });
+		jdbcTemplate.execute("select set_config('cia.encrypt.key', '" + key + "', false)");
 	}
 
 }
