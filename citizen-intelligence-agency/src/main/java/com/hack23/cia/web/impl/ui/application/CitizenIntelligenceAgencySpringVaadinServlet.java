ee47a348393375622ffdd3b1d5991a848cd39d0a
pether.sorling@gmail.com <pether.sorling@gmail.com>
2018-12-10 22:06:21 +0100

    cleanup

--- citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/CitizenIntelligenceAgencySpringVaadinServlet.java
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/CitizenIntelligenceAgencySpringVaadinServlet.java
@@ -27,21 +27,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.DefaultUIProvider;
-import com.vaadin.server.ServiceException;
-import com.vaadin.server.SessionInitEvent;
-import com.vaadin.server.SessionInitListener;
-import com.vaadin.server.UICreateEvent;
 import com.vaadin.server.UIProvider;
 import com.vaadin.server.VaadinServletService;
 import com.vaadin.server.VaadinSession;
-import com.vaadin.spring.internal.SpringViewDisplayRegistrationBean;
-import com.vaadin.spring.internal.UIID;
 import com.vaadin.spring.internal.UIScopeImpl;
 import com.vaadin.spring.internal.VaadinSessionScope;
-import com.vaadin.spring.server.SpringUIProvider;
 import com.vaadin.spring.server.SpringVaadinServlet;
-import com.vaadin.ui.UI;
-import com.vaadin.util.CurrentInstance;
 
 /**
  * The Class CitizenIntelligenceAgencySpringVaadinServlet.
@@ -56,23 +47,14 @@ public final class CitizenIntelligenceAgencySpringVaadinServlet extends SpringVa
 	@Override
 	protected void servletInitialized() throws ServletException {
 		VaadinServletService service = getService();
-		service.addSessionInitListener(new SessionInitListener() {
-
-			private static final long serialVersionUID = -6307820453486668084L;
-
-			@Override
-			public void sessionInit(SessionInitEvent sessionInitEvent) throws ServiceException {
-				final VaadinSession session = sessionInitEvent.getSession();
-				for (UIProvider provider : session.getUIProviders()) {
-					// use canonical names as these may have been loaded with
-					// different classloaders
-					if (DefaultUIProvider.class.getCanonicalName().equals(provider.getClass().getCanonicalName())) {
-						session.removeUIProvider(provider);
-					}
+		service.addSessionInitListener(event -> {
+			final VaadinSession session = event.getSession();
+			for (UIProvider provider : session.getUIProviders()) {
+				if (DefaultUIProvider.class.getCanonicalName().equals(provider.getClass().getCanonicalName())) {
+					session.removeUIProvider(provider);
 				}
-
-				session.addUIProvider(new CustomSpringUIProvider(session));
 			}
+			session.addUIProvider(new CustomSpringUIProvider(session));
 		});
 		service.addSessionDestroyListener(event -> {
 			VaadinSession session = event.getSession();
@@ -80,66 +62,11 @@ public final class CitizenIntelligenceAgencySpringVaadinServlet extends SpringVa
 			VaadinSessionScope.cleanupSession(session);
 		});
 	}
-	
-	 @Override
-	  protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		 
-		 response.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
-	     super.service(request, response);
-	  }
 
-	/**
-	 * The Class CustomSpringUIProvider.
-	 */
-	private static class CustomSpringUIProvider extends SpringUIProvider {
-
-		/** The Constant serialVersionUID. */
-		private static final long serialVersionUID = 1L;
-
-		/** The spring view display registration bean. */
-		private final SpringViewDisplayRegistrationBean springViewDisplayRegistrationBean = new SpringViewDisplayRegistrationBean();
-
-		/**
-		 * Instantiates a new custom spring UI provider.
-		 *
-		 * @param vaadinSession the vaadin session
-		 */
-		public CustomSpringUIProvider(VaadinSession vaadinSession) {
-			super(vaadinSession);
-		}
-
-		@Override
-		public UI createInstance(UICreateEvent event) {
-			final Class<UIID> key = UIID.class;
-			final UIID identifier = new UIID(event);
-			CurrentInstance.set(key, identifier);
-			try {
-				logger.debug("Creating a new UI bean of class [{}] with identifier [{}]",
-						event.getUIClass().getCanonicalName(), identifier);
-				UI ui = getWebApplicationContext().getBean(event.getUIClass());
-
-				getSpringViewDisplayRegistrationBean().setBeanClass(event.getUIClass());
-
-				configureNavigator(ui);
-				return ui;
-			} finally {
-				CurrentInstance.set(key, null);
-			}
-		}
-
-		@Override
-		protected Object findSpringViewDisplay(UI ui) {
-			return getSpringViewDisplayRegistrationBean().getSpringViewDisplay(getWebApplicationContext());
-		}
-
-		/**
-		 * Gets the spring view display registration bean.
-		 *
-		 * @return the spring view display registration bean
-		 */
-		private SpringViewDisplayRegistrationBean getSpringViewDisplayRegistrationBean() {
-			return springViewDisplayRegistrationBean;
-
-		}
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+		super.service(request, response);
 	}
-
 }
---
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/CustomSpringUIProvider.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2014 James Pether Sörling
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *	$Id$
+ *  $HeadURL$
+*/
+package com.hack23.cia.web.impl.ui.application;
+
+import com.vaadin.server.UICreateEvent;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.spring.internal.SpringViewDisplayRegistrationBean;
+import com.vaadin.spring.internal.UIID;
+import com.vaadin.spring.server.SpringUIProvider;
+import com.vaadin.ui.UI;
+import com.vaadin.util.CurrentInstance;
+
+/**
+ * The Class CustomSpringUIProvider.
+ */
+public class CustomSpringUIProvider extends SpringUIProvider {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/** The spring view display registration bean. */
+	private final SpringViewDisplayRegistrationBean springViewDisplayRegistrationBean = new SpringViewDisplayRegistrationBean();
+
+	/**
+	 * Instantiates a new custom spring UI provider.
+	 *
+	 * @param vaadinSession the vaadin session
+	 */
+	public CustomSpringUIProvider(VaadinSession vaadinSession) {
+		super(vaadinSession);
+	}
+
+	@Override
+	public UI createInstance(UICreateEvent event) {
+		final Class<UIID> key = UIID.class;
+		final UIID identifier = new UIID(event);
+		CurrentInstance.set(key, identifier);
+		try {
+			logger.debug("Creating a new UI bean of class [{}] with identifier [{}]",
+					event.getUIClass().getCanonicalName(), identifier);
+			UI ui = getWebApplicationContext().getBean(event.getUIClass());
+
+			getSpringViewDisplayRegistrationBean().setBeanClass(event.getUIClass());
+
+			configureNavigator(ui);
+			return ui;
+		} finally {
+			CurrentInstance.set(key, null);
+		}
+	}
+
+	@Override
+	protected Object findSpringViewDisplay(UI ui) {
+		return getSpringViewDisplayRegistrationBean().getSpringViewDisplay(getWebApplicationContext());
+	}
+
+	/**
+	 * Gets the spring view display registration bean.
+	 *
+	 * @return the spring view display registration bean
+	 */
+	private SpringViewDisplayRegistrationBean getSpringViewDisplayRegistrationBean() {
+		return springViewDisplayRegistrationBean;
+
+	}
+}
