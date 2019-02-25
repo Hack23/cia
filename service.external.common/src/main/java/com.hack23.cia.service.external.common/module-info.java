63395c16ac71e31a19c518cf92c1514dee334b2b
pether.sorling@gmail.com <pether.sorling@gmail.com>
2019-02-25 22:26:03 +0100

    jdk11

--- service.external.common/src/main/java/com.hack23.cia.service.external.common/module-info.java
+++ service.external.common/src/main/java/com.hack23.cia.service.external.common/module-info.java
@@ -1,10 +1,7 @@
-module com.hack23.cia.service.external.common {
+open module com.hack23.cia.service.external.common {
 	exports com.hack23.cia.service.external.common.api;
 	exports com.hack23.cia.service.external.common.impl;
-	
-	opens com.hack23.cia.service.external.common.api to spring.aop, spring.core, spring.beans, spring.context, spring.context.support;
-	opens com.hack23.cia.service.external.common.impl to spring.aop, spring.core, spring.beans, spring.context, spring.context.support;
-		
+			
 	requires org.slf4j;
 	requires java.xml.bind;
 	requires jdom2;
