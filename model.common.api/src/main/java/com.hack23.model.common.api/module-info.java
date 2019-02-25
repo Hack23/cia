f9265b6afa3668d52cf0ffb37a27f333ccbfada9
pether.sorling@gmail.com <pether.sorling@gmail.com>
2019-02-25 20:27:12 +0100

    prepare jdk11 source/target

--- model.common.api/src/main/java/com.hack23.model.common.api/module-info.java
+++ model.common.api/src/main/java/com.hack23.model.common.api/module-info.java
@@ -1,3 +1,4 @@
 module com.hack23.model.common.api {
  exports com.hack23.cia.model.common.api;
+ opens com.hack23.cia.model.common.api;
 }
--- model.common.impl/src/main/java/com.hack23.cia.model.common.impl/module-info.java
+++ model.common.impl/src/main/java/com.hack23.cia.model.common.impl/module-info.java
@@ -3,6 +3,7 @@ module com.hack23.cia.model.common.impl {
 	requires java.xml.bind;
 	requires org.slf4j;
 	requires org.apache.commons.lang3;
+	opens com.hack23.cia.model.common.impl;
 
 	exports com.hack23.cia.model.common.impl;
 }
