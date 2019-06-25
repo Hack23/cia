6cb5b49a1932d13c55d2d596bbbc8eaba1c93cab
pether.sorling@gmail.com <pether.sorling@gmail.com>
2019-06-26 00:02:21 +0200

    missing class

---
+++ service.external.riksdagen/src/main/java/com/hack23/cia/service/external/riksdagen/impl/UrlHelper.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2010-2019 James Pether Sörling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *	$Id$
+ *  $HeadURL$
+*/
+package com.hack23.cia.service.external.riksdagen.impl;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * The Class UrlHelper.
+ */
+public final class UrlHelper {
+
+	/**
+	 * Url encode.
+	 *
+	 * @param id the id
+	 * @return the string
+	 */
+	public static String urlEncode(final String id) {
+		try {
+			return URLEncoder.encode(id, StandardCharsets.UTF_8.toString());
+		} catch (UnsupportedEncodingException e) {
+			return id;
+		}
+	}
+
+}
