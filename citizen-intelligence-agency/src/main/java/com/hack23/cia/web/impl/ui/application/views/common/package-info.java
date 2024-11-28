e211a304f2aaac3d70036673405409d64ca9af7a
James Pether Sörling <pethers@users.noreply.github.com>
2024-11-28 22:08:53 +0100

    Update package-info.java files with descriptions (#6744)
    Update package-info.java files with detailed descriptions for packages in the Citizen Intelligence Agency web application.
    
    * **Admin Package:**
      - Update the package description to explain the purpose of the package.
      - Mention key classes and interfaces such as AgentOperationView, AdminDataSummaryView, AdminAgencyView, etc.
      - Describe dependencies and relationships with other packages and frameworks.
    
    * **Page Mode Package:**
      - Update the package description to explain the purpose of the package.
      - Mention key classes and interfaces such as AbstractBasicPageModContentFactoryImpl, AbstractItemPageModContentFactoryImpl, PageModeContentFactory, etc.
      - Describe dependencies and relationships with other packages and frameworks.
    
    * **Rows Package:**
      - Update the package description to explain the purpose of the package.
      - Mention key classes and interfaces such as RowUtil.
      - Describe dependencies and relationships with other packages and frameworks.
    
    * **Common Package:**
      - Update the package description to explain the purpose of the package.
      - Mention key classes and interfaces such as AbstractView, DashboardView, MainView, ListPropertyConverter, etc.
      - Describe dependencies and relationships with other packages and frameworks.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Hack23/cia?shareId=XXXX-XXXX-XXXX-XXXX).

--- citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/package-info.java
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/package-info.java
@@ -1,4 +1,23 @@
 /**
- * Provides...
+ * This package provides common classes and interfaces for the Citizen Intelligence Agency web application.
+ * 
+ * Key classes and interfaces:
+ * - AbstractView: Base class for views.
+ * - DashboardView: Main view class for the dashboard.
+ * - MainView: Main view class for the application.
+ * - ListPropertyConverter: Converter for handling list properties.
+ * 
+ * Dependencies and relationships:
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.pagemode for PageModeContentFactory.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.rows for RowUtil.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.converters for ListPropertyConverter.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.sizing for ContentRatio.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.labelfactory for LabelFactory.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.menufactory.api for ApplicationMenuItemFactory.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.pagelinks.api for PageLinkFactory.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.pageclicklistener for LogoutClickListener.
+ * - Depends on com.hack23.cia.web.impl.ui.application.util for UserContextUtil.
+ * - Depends on com.vaadin for Vaadin framework classes and interfaces.
+ * - Depends on org.springframework for Spring framework classes and interfaces.
  */
 package com.hack23.cia.web.impl.ui.application.views.common;
--- citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/rows/package-info.java
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/rows/package-info.java
@@ -1,4 +1,12 @@
 /**
- * Provides...
+ * This package provides utility classes for creating and managing rows within the Citizen Intelligence Agency web application.
+ * 
+ * Key classes and interfaces:
+ * - RowUtil: Utility class for creating and managing rows.
+ * 
+ * Dependencies and relationships:
+ * - Depends on com.jarektoro.responsivelayout for responsive layout classes.
+ * - Depends on com.vaadin for Vaadin framework classes and interfaces.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.sizing for content sizing.
  */
 package com.hack23.cia.web.impl.ui.application.views.common.rows;
