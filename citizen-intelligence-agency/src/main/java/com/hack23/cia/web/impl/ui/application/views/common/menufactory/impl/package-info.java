84aa5fb56165ccda649a77488041e221c840ea33
James Pether Sörling <pethers@users.noreply.github.com>
2024-11-29 18:21:27 +0100

    Update package-info.java descriptions
    Update package-info.java files with detailed descriptions for various packages in the com.hack23.cia.web module.
    
    * Update `citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/sizing/package-info.java` to provide a detailed purpose, key classes, and dependencies.
    * Update `citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/user/common/package-info.java` to provide a detailed purpose, key classes, and dependencies.
    * Update `citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/chartfactory/api/package-info.java` to provide a detailed purpose, key classes, and dependencies.
    * Update `citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/chartfactory/impl/package-info.java` to provide a detailed purpose, key classes, and dependencies.
    * Update `citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/pagelinks/impl/package-info.java` to provide a detailed purpose, key classes, and dependencies.
    * Update `citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/menufactory/impl/package-info.java` to provide a detailed purpose, key classes, and dependencies.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Hack23/cia?shareId=XXXX-XXXX-XXXX-XXXX).

--- citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/menufactory/impl/package-info.java
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/menufactory/impl/package-info.java
@@ -1,4 +1,31 @@
 /**
- * Provides...
+ * This package provides classes and interfaces for creating and managing menu items within the Citizen Intelligence Agency web application.
+ * 
+ * Key classes and interfaces:
+ * - AbstractMenuItemFactoryImpl: Base class for creating menu items.
+ * - ApplicationMenuItemFactoryImpl: Implementation of the ApplicationMenuItemFactory interface.
+ * - BallotMenuItemFactoryImpl: Implementation of the BallotMenuItemFactory interface.
+ * - CommitteeMenuItemFactoryImpl: Implementation of the CommitteeMenuItemFactory interface.
+ * - CommitteeRankingMenuItemFactoryImpl: Implementation of the CommitteeRankingMenuItemFactory interface.
+ * - CountryMenuItemFactoryImpl: Implementation of the CountryMenuItemFactory interface.
+ * - DocumentMenuItemFactoryImpl: Implementation of the DocumentMenuItemFactory interface.
+ * - GovernmentBodyMenuItemFactoryImpl: Implementation of the GovernmentBodyMenuItemFactory interface.
+ * - GovernmentBodyRankingMenuItemFactoryImpl: Implementation of the GovernmentBodyRankingMenuItemFactory interface.
+ * - MinistryMenuItemFactoryImpl: Implementation of the MinistryMenuItemFactory interface.
+ * - MinistryRankingMenuItemFactoryImpl: Implementation of the MinistryRankingMenuItemFactory interface.
+ * - ParliamentMenuItemFactoryImpl: Implementation of the ParliamentMenuItemFactory interface.
+ * - PartyMenuItemFactoryImpl: Implementation of the PartyMenuItemFactory interface.
+ * - PartyRankingMenuItemFactoryImpl: Implementation of the PartyRankingMenuItemFactory interface.
+ * - PoliticianMenuItemFactoryImpl: Implementation of the PoliticianMenuItemFactory interface.
+ * - PoliticianRankingMenuItemFactoryImpl: Implementation of the PoliticianRankingMenuItemFactory interface.
+ * - UserHomeMenuItemFactoryImpl: Implementation of the UserHomeMenuItemFactory interface.
+ * 
+ * Dependencies and relationships:
+ * - Depends on com.hack23.cia.web.impl.ui.application.util for UserContextUtil.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.pagelinks.api for PageModeMenuCommand.
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.viewnames for various view names and page modes.
+ * - Depends on com.jarektoro.responsivelayout for responsive layout classes.
+ * - Depends on com.vaadin for Vaadin framework classes and interfaces.
+ * - Depends on org.springframework for Spring framework classes and interfaces.
  */
 package com.hack23.cia.web.impl.ui.application.views.common.menufactory.impl;
--- citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/pagelinks/impl/package-info.java
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/common/pagelinks/impl/package-info.java
@@ -1,4 +1,15 @@
 /**
- * Provides...
+ * This package provides classes and interfaces for creating and managing page links within the Citizen Intelligence Agency web application.
+ * 
+ * Key classes and interfaces:
+ * - ExternalAttachmentDownloadLink: A class for creating download links for external attachments.
+ * - PageLinkFactoryImpl: Implementation of the PageLinkFactory interface.
+ * - StreamSourceImplementation: A class for handling stream resources for external attachments.
+ * 
+ * Dependencies and relationships:
+ * - Depends on com.hack23.cia.web.impl.ui.application.views.common.viewnames for view names and page modes.
+ * - Depends on com.hack23.cia.web.impl.ui.application.action for handling page actions.
+ * - Depends on com.vaadin for Vaadin framework classes and interfaces.
+ * - Depends on org.springframework for Spring framework classes and interfaces.
  */
 package com.hack23.cia.web.impl.ui.application.views.common.pagelinks.impl;
