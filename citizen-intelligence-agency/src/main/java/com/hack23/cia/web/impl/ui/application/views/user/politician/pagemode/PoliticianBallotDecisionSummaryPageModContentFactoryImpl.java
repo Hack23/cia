139c0e6569f3d77ffe877158156fdba0e72a2d05
James Pether Sörling <pethers@users.noreply.github.com>
2024-12-04 20:53:35 +0100

    Revert "Update panel caption to use embeddedId.concern (#6780)"
    This reverts commit 811baab8c04f9aa938c26dc46610edb102b9590a.

--- citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/user/politician/pagemode/PoliticianBallotDecisionSummaryPageModContentFactoryImpl.java
+++ citizen-intelligence-agency/src/main/java/com/hack23/cia/web/impl/ui/application/views/user/politician/pagemode/PoliticianBallotDecisionSummaryPageModContentFactoryImpl.java
@@ -7,13 +7,14 @@
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *distributed under the License is distributed on an "AS IS" BASIS,
- *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *See the License for the specific language governing permissions and
- *limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *$Id$
- *$HeadURL$
+ *	$Id$
+ *  $HeadURL$
 */
 package com.hack23.cia.web.impl.ui.application.views.user.politician.pagemode;
 
@@ -97,7 +98,6 @@ public final class PoliticianBallotDecisionSummaryPageModContentFactoryImpl
 				COMMITTEE_BALLOT_DECISION_POLITICIAN_SUMMARY, NESTED_PROPERTIES, COLUMN_ORDER, HIDE_COLUMNS, LISTENER,
 				BALLOT_ID, null);
 
-		panel.setCaption(new StringBuilder().append("Politician Ballot Decision Summary for concern: ").append(viewRiksdagenPolitician.getEmbeddedId().getConcern()).toString());
 
 		pageCompleted(parameters, panel, pageId, viewRiksdagenPolitician);
 
