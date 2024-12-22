1bb4b8e91851745118a0f77d4c7349fe6c6b8a08
James Pether Sörling <pethers@users.noreply.github.com>
2024-12-22 21:10:35 +0100

    Add missing package-info.java files with descriptions (#6883)
    Add detailed descriptions and annotations to package-info.java files in various packages.
    
    * Update `model.common.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.riksdagen.documentcontent.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.riksdagen.dokumentlista.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.riksdagen.dokumentstatus.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.riksdagen.person.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.riksdagen.personlista.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.riksdagen.utskottsforslag.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.riksdagen.votering.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.riksdagen.voteringlista.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.val.kommunvalkrets.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.val.landstingvalkrets.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.val.partier.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.val.riksdagsvalkrets.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.worldbank.countries.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.worldbank.data.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.worldbank.indicators.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.external.worldbank.topic.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    * Update `model.internal.application.user.impl` package description to provide a detailed explanation of the package's purpose and key classes. Include `@XmlSchema` annotation.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Hack23/cia?shareId=XXXX-XXXX-XXXX-XXXX).

--- model.common.impl/src/main/java/com/hack23/cia/model/common/impl/package-info.java
+++ model.common.impl/src/main/java/com/hack23/cia/model/common/impl/package-info.java
@@ -1,4 +1,11 @@
 /**
- * Provides...
+ * This package contains common model implementations used across the application.
+ *
+ * Key classes:
+ * - AbstractModelObjectImpl: Base class for model objects providing common implementations for equals, hashCode, and toString methods.
+ *
+ * Annotations:
+ * - @XmlSchema: Defines the XML namespace and element form default for the package.
  */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://common.model.cia.hack23.com/impl", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.hack23.cia.model.common.impl;
