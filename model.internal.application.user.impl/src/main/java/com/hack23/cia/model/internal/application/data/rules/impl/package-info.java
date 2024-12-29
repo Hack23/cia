67ca92e4d6622b764be85b84276875a83d9161fe
James Pether Sörling <pethers@users.noreply.github.com>
2024-12-29 12:56:58 +0100

    Update Javadoc comments in package-info.java files
    Update Javadoc comments in package-info.java files to provide a clear purpose of the packages based on the classes in the packages.
    
    * **model.common.impl/src/main/java/com/hack23/cia/model/common/impl/xml/package-info.java**
      - Update Javadoc to describe XML-related utilities and adapters.
      - Mention key classes and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/document/impl/package-info.java**
      - Update Javadoc to describe document data handling.
      - Mention key classes and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/impl/package-info.java**
      - Update Javadoc to describe general data handling.
      - Mention key classes and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/party/impl/package-info.java**
      - Update Javadoc to describe party data handling.
      - Mention key classes and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/rules/impl/package-info.java**
      - Update Javadoc to describe rules data handling.
      - Mention key classes and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/secure/impl/package-info.java**
      - Update Javadoc to describe secure data handling.
      - Mention key class and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/system/impl/package-info.java**
      - Update Javadoc to describe system data handling.
      - Mention key classes and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/user/impl/package-info.java**
      - Update Javadoc to describe user-related functionalities.
      - Mention @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/ministry/impl/package-info.java**
      - Update Javadoc to describe ministry data handling.
      - Mention key classes and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/audit/impl/package-info.java**
      - Update Javadoc to describe audit data handling.
      - Mention key classes and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/politician/impl/package-info.java**
      - Update Javadoc to describe politician data handling.
      - Mention key classes and @XmlSchema annotation.
    
    * **model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/committee/impl/package-info.java**
      - Update Javadoc to describe committee data handling.
      - Mention key classes and @XmlSchema annotation.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Hack23/cia?shareId=XXXX-XXXX-XXXX-XXXX).

--- model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/rules/impl/package-info.java
+++ model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/data/rules/impl/package-info.java
@@ -1,4 +1,13 @@
 /**
- * Provides...
+ * This package contains implementations for handling rules data within the internal application.
+ *
+ * Key classes:
+ * - ResourceType: Enum representing different resource types.
+ * - RuleViolation: Represents a rule violation.
+ * - Status: Enum representing different statuses.
+ *
+ * Annotations:
+ * - @XmlSchema: Defines the XML namespace and element form default for the package.
  */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://rules.data.application.internal.model.cia.hack23.com/impl")
 package com.hack23.cia.model.internal.application.data.rules.impl;
--- model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/user/impl/package-info.java
+++ model.internal.application.user.impl/src/main/java/com/hack23/cia/model/internal/application/user/impl/package-info.java
@@ -11,6 +11,9 @@
  * - Depends on com.hack23.cia.service.api for service-related classes and interfaces.
  * - Depends on org.springframework for Spring framework classes and interfaces.
  *
+ * Annotations:
+ * - @XmlSchema: Defines the XML namespace and element form default for the package.
+ *
  * The package is responsible for providing the necessary structure and implementation for managing user-related functionalities within the internal application of the Citizen Intelligence Agency. The key classes and interfaces within this package manage various aspects of user management, such as user entities, user-related operations, and their implementation. The package also has dependencies on other packages for specific functionalities, such as data-related classes, service-related classes, and Spring framework.
  */
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://user.application.internal.model.cia.hack23.com/impl")
