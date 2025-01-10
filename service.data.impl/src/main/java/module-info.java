d9814f5416044e5a409e9171750cb0aa0c4d8c52
James Pether Sörling <pethers@users.noreply.github.com>
2025-01-10 01:31:32 +0100

    Update module-info.java

--- service.data.impl/src/main/java/module-info.java
+++ service.data.impl/src/main/java/module-info.java
@@ -37,7 +37,7 @@ open module com.hack23.cia.service.data.impl {
 	requires java.sql;
 	requires java.desktop;
 	requires java.annotation;
-	requires org.apache.commons.beanutils2;
+	requires org.apache.commons.beanutils;
 	requires org.slf4j;
 	requires spring.context;
 	requires spring.context.support;
