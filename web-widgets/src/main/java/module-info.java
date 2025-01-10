3eed2b3d39adff5b8a6b34c1f266d160a4476b6e
pethers <pether.sorling@gmail.com>
2025-01-10 15:47:29 +0100

    mod3

---
+++ web-widgets/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module ccom.hack23.cia.web.widgets.charts {
+	exports com.hack23.cia.web.widgets.charts;
+	
+	requires vaadin.compatibility.shared;
+	requires vaadin.compatibility.server;
+	requires vaadin.shared;
+	requires vaadin.server;
+	requires org.apache.commons.lang3;
+
+}
