df4b60323ffdb0c1070231c01c8279c87582f225
James Pether Sörling <pethers@users.noreply.github.com>
2025-01-12 19:08:01 +0100

    feat(postgresql): upgrade to PostgreSQL 16 and enhance initialization script for improved security and reliability

--- citizen-intelligence-agency/src/test/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.hack23.cia.systemintegrationtest {
-    requires com.hack23.cia.web;
-    requires org.eclipse.jetty.server;
-    requires org.eclipse.jetty.util;
-    requires org.slf4j;
-}
