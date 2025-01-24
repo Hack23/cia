5a20e825060db9c9414fdbacca8a2c3fb0949f1a
James Pether Sörling <pethers@users.noreply.github.com>
2025-01-24 19:51:51 +0100

    refactor: clean up devcontainer and vscode configurations; update JAXB unmarshalling in tests

--- model.external.riksdagen.dokumentlista.impl/src/test/java/com/hack23/cia/model/external/riksdagen/dokumentlista/impl/DocumentContainerElementTest.java
+++ model.external.riksdagen.dokumentlista.impl/src/test/java/com/hack23/cia/model/external/riksdagen/dokumentlista/impl/DocumentContainerElementTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 import org.junit.Test;
@@ -14,34 +13,24 @@ import org.junit.Test;
 public class DocumentContainerElementTest {
 
     @Test
-    public void testUnmarshalXmlToDocumentContainerElement() {
-        try {
+    public void testUnmarshalXmlToDocumentContainerElement() throws Exception {
             // Create a JAXB context for the DocumentContainerElement class
-            final JAXBContext context = JAXBContext.newInstance(DocumentContainerElement.class);
+            final JAXBContext context = JAXBContext.newInstance(DocumentContainerElement.class,DocumentElement.class);
 
             // Create an unmarshaller
             final Unmarshaller unmarshaller = context.createUnmarshaller();
-            unmarshaller.setEventHandler(new javax.xml.bind.helpers.DefaultValidationEventHandler());
+            //unmarshaller.setEventHandler(new javax.xml.bind.helpers.DefaultValidationEventHandler());
 
 
             // Path to the XML file to be read
             final File file = new File("src/test/samples/documentlista-2025b.xml");
 
             // Unmarshal the XML file into a DocumentContainerElement object
-            final DocumentContainerElement documentContainerElement = (DocumentContainerElement) ((javax.xml.bind.JAXBElement) unmarshaller.unmarshal(file)).getValue();
+            final DocumentContainerElement documentContainerElement = (DocumentContainerElement) unmarshaller.unmarshal(file);
 
             // Assert that the object is not null
             assertNotNull(documentContainerElement);
             assertNotNull(documentContainerElement.getDokument());
             assertTrue(documentContainerElement.getDokument().size() > 0);
-
-            System.out.println(documentContainerElement.getDokument());
-
-            // Additional assertions can be added here to verify the contents of the object
-            // For example: assertEquals("expected value", documentContainerElement.getDatum());
-
-        } catch (final JAXBException e) {
-            e.printStackTrace();
-        }
     }
 }
